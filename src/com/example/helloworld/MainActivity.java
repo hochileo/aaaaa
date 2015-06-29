d1c26a6dbcbc1567a08c8c207fb2b929d90a91e2
Lâm Nguyễn <lamnt.hust@gmail.com>
2015-06-29 15:56:50 +0700

    v2

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -6,7 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+//tôi là Lâm
+	//and you 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
